ff3595fae7a591101105ee51984996efa1c7a8bd
rabiilfarakh <rabiilfarakh2816@gmail.com>
2024-04-01 15:55:23 +0000

    add modification

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,5 +1,6 @@
 <template>
-<table class="divide-y divide-gray-200 border-solid border-2 border-black">
+<button class="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out">Add</button>
+<table class="mt-1 divide-y divide-gray-200 border-solid border-2 border-black">
     <thead>
         <tr>
             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ID</th>
@@ -13,9 +14,9 @@
     <tbody class="bg-white divide-y divide-gray-200">
         <tr v-for="progression in progressions" :key="progression.id">
             <td class="px-6 py-4 whitespace-nowrap">{{progression.id}}</td>
-            <td class="px-6 py-4 whitespace-nowrap">{{progression.poids}}</td>
-            <td class="px-6 py-4 whitespace-nowrap">{{progression.height}}</td>
-            <td class="px-6 py-4 whitespace-nowrap">{{progression.biceps}}</td>
+            <td class="px-6 py-4 whitespace-nowrap">{{progression.poids}} <span class="text-gray-600">kg</span></td>
+            <td class="px-6 py-4 whitespace-nowrap">{{progression.height}} <span class="text-gray-600">cm</span></td>
+            <td class="px-6 py-4 whitespace-nowrap">{{progression.biceps}} <span class="text-gray-600">cm</span></td>
             <td class="px-6 py-4 whitespace-nowrap">
                 <span v-if="progression.status === 'Non terminé'" class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
                     {{ progression.status }}
@@ -26,7 +27,7 @@
             </td>
 
             <td class="px-6 py-4 whitespace-nowrap">
-                <button class="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out">Edit</button>
+                <button class="px-4 py-2 font-medium text-white bg-yellow-300 rounded-md hover:bg-yellow-200 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out">Update</button>
                 <button class="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out">Delete</button>
             </td>
         </tr>
